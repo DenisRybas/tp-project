49173ba35b325752b11eb181db8652ebc9c48416
Denis Rybas <61991100+DenisRybas@users.noreply.github.com>
2022-04-24 16:13:00 +0300

    template diaries routes added

---
+++ awareness/template_diaries/utils.py
@@ -0,0 +1,9 @@
+from awareness.models import Theme
+
+
+def get_random_theme_id():
+    import time
+    import random
+    random.seed(time.time())
+    num_of_themes = Theme.query.count()
+    return random.randint(1, num_of_themes)
