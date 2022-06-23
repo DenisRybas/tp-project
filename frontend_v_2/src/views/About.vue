2c1b2f5977c5b347546a494a41ce79a17f3ed8f1
Denis Rybas <61991100+DenisRybas@users.noreply.github.com>
2022-06-23 12:44:27 +0300

    about, contacts updated

---
+++ frontend_v_2/src/views/About.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container bg-white center-this">
+    <h1 class="ps-2 display-4">Awareness</h1>
+    <hr>
+    <div class="px-4 py-4">
+      <p>
+        Ваш персональный помощник в повышении осознанности путем ведения различных видов дневников, а также отслеживания
+        привычек.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About"
+}
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+
+.center-this {
+    margin-top: 10%;
+}
+</style>
---
+++ frontend_v_2/src/views/Contacts.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container bg-white center-this">
+    <h1 class="ps-2 display-4">{{ 'Контакты' }}</h1>
+    <hr>
+    <div class="px-4 py-4">
+      <p>
+        {{ 'Соболь Илья' }}: <a href="mailto:ilja-sobol.sobol@yandex.ru" target="_blank">ilja-sobol.sobol@yandex.ru</a>
+      </p>
+      <p>
+        {{ 'Рыбас Денис' }}: <a href="mailto:denis2002.rybas@gmail.com" target="_blank">denis2002.rybas@gmail.com</a>
+      </p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contacts"
+}
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: #0d6efd;
+}
+
+h1 {
+  text-align: center;
+}
+
+.center-this {
+    margin-top: 10%;
+}
+</style>
